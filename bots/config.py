78571cf67018b47b2c891d937321d978f3970fc0
Erin Claudio <eclaudio@gmail.com>
2020-01-15 13:52:31 -0500

    small amount of code cleaned up

--- bots/config.py
+++ bots/config.py
@@ -6,10 +6,10 @@ logger = logging.getLogger()
 
 
 def create_api():
-    consumer_key = os.getenv("Gf9KC0aZoiqu4eBC2YaYrYluw")
-    consumer_secret = os.getenv("U4w9dwJ5ta9TK6YZ2AuEgxIAXZzyl1HCROorrq2m3EspnocfOS")
-    access_token = os.getenv("1217447530813890560-XIck0nW7qaRreIs5Ef1NUUSsqlgKjQ")
-    access_token_secret = os.getenv("uU3pkdPRuOGj0ENzdbjCUKzY7CaRIRWmrIxJAOxN308S7")
+    consumer_key = os.getenv("Gf9KC0aZoiqu4eBC2YaYr")
+    consumer_secret = os.getenv("U4w9dwJ5ta9TK6YZ2AuEgxIAXZzyl1HCROorrq2m")
+    access_token = os.getenv("1217447530813890560-XIck0nW7qaRreIs5Ef1NUU")
+    access_token_secret = os.getenv("uU3pkdPRuOGj0ENzdbjCUKzY7CaRIRWmrI")
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)

915f0eb068d5d65558a9c030d84230244e201ec1
Erin Claudio <eclaudio@gmail.com>
2020-01-16 15:36:17 -0500

    working version

--- bots/favretweet.py
+++ bots/favretweet.py
@@ -1,7 +1,8 @@
 import tweepy
 import logging
 from config import create_api
-#import json
+
+# import json
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
@@ -16,7 +17,7 @@ class FavRetweetListener(tweepy.StreamListener):
     def on_status(self, tweet):
         logger.info(f"Processing tweet id {tweet.id}")
         if tweet.in_reply_to_status_id is not None or \
-            tweet.user.id == self.me.id:
+                tweet.user.id == self.me.id:
             return
         if not tweet.favorited:
             try:
@@ -32,6 +33,7 @@ class FavRetweetListener(tweepy.StreamListener):
     def on_error(self, status):
         logger.error(status)
 
+
 def main(keywords):
     api = create_api()
     tweets_listener = FavRetweetListener(api)
@@ -40,4 +42,4 @@ def main(keywords):
 
 
 if __name__ == "__main__":
-    main(["Python", "Tweepy"])
+    main(["#learninpublic"])

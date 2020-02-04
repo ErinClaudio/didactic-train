5038ecea191fe06b56a70feaa91cff216b624581
Erin Claudio <eclaudio@gmail.com>
2020-02-04 14:16:06 -0500

    Procfile added

--- bots/config.py
+++ bots/config.py
@@ -1,15 +1,15 @@
 import tweepy
 import logging
-import os
+from os import environ
 
 logger = logging.getLogger()
 
 
 def create_api():
-    consumer_key = os.getenv("CONSUMER_KEY")
-    consumer_secret = os.getenv("CONSUMER_SECRET")
-    access_token = os.getenv("ACCESS_TOKEN")
-    access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+    consumer_key = environ("CONSUMER_KEY")
+    consumer_secret = environ("CONSUMER_SECRET")
+    access_token = environ("ACCESS_TOKEN")
+    access_token_secret = environ("ACCESS_TOKEN_SECRET")
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
